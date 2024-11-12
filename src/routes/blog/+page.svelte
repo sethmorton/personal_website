51113a11c0658f3ce9721f2b6a3b8cefa7a5661e
Seth Morton <smphotography39@gmail.com>
2024-11-12 14:03:18 -0500

    flex over absolute

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,10 +1,14 @@
 <script>
   import { goto } from '$app/navigation';
 </script>
-<div class="absolute top-0 left-0 p-8">
-    <button class="text-blue-600 hover:text-blue-800" on:click={() => goto('/')}>Back</button>
-  </div>
-<div class="relative w-screen h-screen flex flex-col justify-center items-center">
-  <h1>blog</h1>
-  <p class="text-gray-500">coming soon</p>
+<div class="flex flex-col w-screen h-screen justify-between">
+    <div class="flex justify-start p-8">
+        <button class="text-blue-600 hover:text-blue-800" on:click={() => goto('/')}>Back</button>
+      </div>
+    <div class="flex flex-col justify-center items-center">
+      <h1>blog</h1>
+      <p class="text-gray-500">coming soon</p>
+    </div>
+    <div></div>
 </div>
+
