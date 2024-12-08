9c032018e91d00f5f7bfdc42e6a6e85b2bb957f7
Seth Morton <smphotography39@gmail.com>
2024-12-08 14:16:39 -0500

    changed node run time

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,11 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-
 	kit: {
-		adapter: adapter({ split: false })
+		adapter: adapter({
+			runtime: 'nodejs20.x'
+		})
 	}
 };
 
