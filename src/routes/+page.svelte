741cdc9b26f45fa6186580ac334cecb78c834218
Seth Morton <smphotography39@gmail.com>
2024-12-08 15:30:26 -0500

    added tracking to personal web links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,70 @@
-<script>
-  import { FileText, Linkedin, Mail } from 'lucide-svelte';
-  
-  let hoveredIcon = $state(null);
+<script lang="ts">
+	import { FileText, Linkedin, Mail } from 'lucide-svelte';
+
+	let hoveredIcon = $state(null);
+	const LINKS = [
+		{
+			name: 'resume',
+			url: 'http://go.sethmorton.xyz/resume'
+		},
+		{
+			name: 'linkedin',
+			url: 'http://go.sethmorton.xyz/linkedin'
+		},
+		{
+			name: 'github',
+			url: 'https://go.sethmorton.xyz/github'
+		}
+	];
 </script>
 
 <svelte:head>
-  <title>Seth Morton</title>
+	<title>Seth Morton</title>
 </svelte:head>
 
-<div class="flex w-screen h-screen flex-col justify-center items-center">
-  <nav class="absolute top-12 left-0 right-0 flex flex-row justify-center gap-5">
-    <a href="/about" 
-       class="p-2.5 cursor-pointer hover:underline hover:text-gray-500">about</a>
-    <a href="/blog" 
-       class="p-2.5 cursor-pointer hover:underline hover:text-gray-500">blog</a>
-  </nav>
+<div class="flex h-screen w-screen flex-col items-center justify-center">
+	<nav class="absolute left-0 right-0 top-12 flex flex-row justify-center gap-5">
+		<a href="/about" class="cursor-pointer p-2.5 hover:text-gray-500 hover:underline">about</a>
+		<a href="/blog" class="cursor-pointer p-2.5 hover:text-gray-500 hover:underline">blog</a>
+	</nav>
 
-  <main class="flex-1 flex flex-col items-center justify-center ">
-    <h1 class="text-2xl font-bold text-center">hi, i'm seth morton</h1>
-    <p class="pt-2.5 text-lg text-center">:)</p>
-    <p class="pt-2.5 text-sm sm:text-lg text-center">
-      I'm building <a href="https://digestibletext.com" class="text-black underline hover:text-gray-500">Digestible Text</a>, 
-      a tool to help you understand what you read.
-    </p>
-    
-    <div class="flex flex-row mt-5">
-      <a href="mailto:sethmorton05@gmail.com" 
-         class="p-2.5 mx-2.5 cursor-pointer transition-transform duration-200 hover:scale-110"
-         onmouseenter={() => hoveredIcon = 'mail'}
-         onmouseleave={() => hoveredIcon = null}>
-        <Mail 
-          size={24} 
-          color={hoveredIcon === 'mail' ? '#808080' : '#000000'} 
-        />
-      </a>
-      
-      <a href="https://utfs.io/f/PZA8aODL9FcWtX2t5yXFRFdpYJG36XyxbLAE02DMTgvOeHhz" 
-         class="p-2.5 mx-2.5 cursor-pointer transition-transform duration-200 hover:scale-110"
-         onmouseenter={() => hoveredIcon = 'resume'}
-         onmouseleave={() => hoveredIcon = null}>
-        <FileText 
-          size={24} 
-          color={hoveredIcon === 'resume' ? '#808080' : '#000000'} 
-        />
-      </a>
-      
-      <a href="https://www.linkedin.com/in/seth-morton-118574242" 
-         class="p-2.5 mx-2.5 cursor-pointer transition-transform duration-200 hover:scale-110"
-         onmouseenter={() => hoveredIcon = 'linkedin'}
-         onmouseleave={() => hoveredIcon = null}>
-        <Linkedin 
-          size={24} 
-          color={hoveredIcon === 'linkedin' ? '#808080' : '#000000'} 
-        />
-      </a>
-    </div>
-  </main>
+	<main class="flex flex-1 flex-col items-center justify-center">
+		<h1 class="text-center text-2xl font-bold">hi, i'm seth morton</h1>
+		<p class="pt-2.5 text-center text-lg">:)</p>
+		<p class="pt-2.5 text-center text-sm sm:text-lg">
+			I'm building <a
+				href="https://digestibletext.com"
+				class="text-black underline hover:text-gray-500">Digestible Text</a
+			>, a tool to help you understand what you read.
+		</p>
+
+		<div class="mt-5 flex flex-row">
+			<a
+				href="mailto:sethmorton05@gmail.com"
+				class="mx-2.5 cursor-pointer p-2.5 transition-transform duration-200 hover:scale-110"
+				onmouseenter={() => (hoveredIcon = 'mail')}
+				onmouseleave={() => (hoveredIcon = null)}
+			>
+				<Mail size={24} color={hoveredIcon === 'mail' ? '#808080' : '#000000'} />
+			</a>
+
+			<a
+				href="hhttp://go.sethmorton.xyz/resume"
+				class="mx-2.5 cursor-pointer p-2.5 transition-transform duration-200 hover:scale-110"
+				onmouseenter={() => (hoveredIcon = 'resume')}
+				onmouseleave={() => (hoveredIcon = null)}
+			>
+				<FileText size={24} color={hoveredIcon === 'resume' ? '#808080' : '#000000'} />
+			</a>
+
+			<a
+				href="http://go.sethmorton.xyz/linkedin"
+				class="mx-2.5 cursor-pointer p-2.5 transition-transform duration-200 hover:scale-110"
+				onmouseenter={() => (hoveredIcon = 'linkedin')}
+				onmouseleave={() => (hoveredIcon = null)}
+			>
+				<Linkedin size={24} color={hoveredIcon === 'linkedin' ? '#808080' : '#000000'} />
+			</a>
+		</div>
+	</main>
 </div>
